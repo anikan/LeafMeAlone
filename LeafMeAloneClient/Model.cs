97581a7fbb53b8a14bc64676719d14d864bfd126
Madaoism <cai_yiming@yahoo.com>
2018-04-15 13:55:05 -0700

    transform

--- LeafMeAloneClient/Model.cs
+++ LeafMeAloneClient/Model.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Shared;
 
 namespace Client
 {
     class Model
     {
+        private Geometry m_ActiveGeo;
+
+
+
         public Model(string filePath)
         {
-          //  if(GraphicsManager.Geo.ContainsKey())
+
         }
     }
 }
---
+++ LeafMeAloneShared/TransformProperties.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SlimDX;
+
+namespace Shared
+{
+    public struct TransformProperties
+    {
+        Vector3 Position;  // location of the model in world coordinates
+        Vector3 Direction; // unit vector pointing to the direction the model is facing
+        Vector3 Scale;     // scale of the model
+    }
+}

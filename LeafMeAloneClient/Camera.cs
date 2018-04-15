60439bcab55128acc9704273c425863b7c0f3c22
Madaoism <cai_yiming@yahoo.com>
2018-04-15 12:58:56 -0700

    updated camera class

--- LeafMeAloneClient/Camera.cs
+++ LeafMeAloneClient/Camera.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SlimDX;
 
 namespace Client
 {
     class Camera
     {
+        public Vector3 CameraUp;
+        public Vector3 CameraLookAt;
+        public Vector3 CameraPosition;
+        public Matrix ViewMatrix;
+
+        public Camera( Vector3 pos, Vector3 lookat, Vector3 up)
+        {
+
+        }
     }
 }

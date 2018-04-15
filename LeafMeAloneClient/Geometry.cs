6890efe2d451432f359006946fd76e9345015f20
Nick Crow <nicholas@nicholasjcrow.com>
2018-04-15 13:41:20 -0700

    Added utility, and geometry working

--- LeafMeAloneClient/Geometry.cs
+++ LeafMeAloneClient/Geometry.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Assimp;
+using Assimp.Configs;
+using Shared;
+using SlimDX;
 namespace Client
 {
     class Geometry
     {
+        private List<Vector3> Vertices;
+        private List<Vector3> Normals;
+        private List<int> Faces;
+
+        private Scene scene;
+
+
+        private AssimpContext importer;
+
+
+        public Geometry(string fileName)
+        {
+            importer = new AssimpContext();
+            scene = importer.ImportFile(fileName);
+            if (scene == null)
+            {
+                throw new FileNotFoundException();
+            }
+            else
+            {
+                foreach (Mesh sceneMesh in scene.Meshes)
+                {
+                    sceneMesh.Vertices.ForEach(vertex =>
+                    {
+                        Vertices.Add(vertex.ToVector3());
+                    });
+                }
+            }
+        }
     }
 }
---
+++ LeafMeAloneShared/Utility.cs
@@ -0,0 +1,13 @@
+﻿using Assimp;
+using SlimDX;
+
+namespace Shared
+{
+    public static class Utility
+    {
+        public static Vector3 ToVector3(this Vector3D vector)
+        {
+            return new Vector3(vector.X,vector.Y,vector.Z);
+        }
+    }
+}

4cb74defcba91535dabad653005f945bc4d19685
Madaoism <cai_yiming@yahoo.com>
2018-05-20 15:15:31 -0700

    finished writing a basic audio system that works for 8bit and 16bit mono/stereo files

--- LeafMeAloneClient/AudioSystem.cs
+++ LeafMeAloneClient/AudioSystem.cs
@@ -34,25 +34,37 @@ namespace Client
         }
 
         /// <summary>
-        /// Move the sound source to the specified src_position in the world coordinate system
+        /// Move the sound source to the specified srcPosition in the world coordinate system
         /// This is necessary for 3D audio effects.
         /// </summary>
         /// <param name="soundSource"> ID of the source </param>
-        /// <param name="src_position"> Location of the source </param>
-        public void UpdateSourcePosition(int soundSource, Vector3 src_position)
+        /// <param name="srcPosition"> Location of the source </param>
+        public void UpdateSourcePosition(int soundSource, Vector3 srcPosition)
         {
-            AL.Source(soundSource, ALSource3f.Position, src_position.X, src_position.Y, src_position.Z);
+            AL.Source(soundSource, ALSource3f.Position, srcPosition.X, srcPosition.Y, srcPosition.Z);
         }
 
-        public void UpdateListenerPosition(Vector3 src_position)
+        /// <summary>
+        /// Update the position of the listener, typically the position of the camera
+        /// </summary>
+        /// <param name="listenerPosition"> new position of the listener </param>
+        public void UpdateListenerPosition(Vector3 listenerPosition)
         {
-
+            AL.Listener(ALListener3f.Position, listenerPosition.X, listenerPosition.Y, listenerPosition.Z);
         }
 
+        /// <summary>
+        /// Generate a new buffer by reading in an audio file
+        /// </summary>
+        /// <param name="fileName"></param>
         private void GenBuffer(string fileName)
         {
             // create a new file descriptor
             var wavfile = SndFile.OpenRead(fileName);
+            if (wavfile.GetError() != SfError.NoError)
+            {
+                return;
+            }
 
             // size of the wav file
             long bufferSize = wavfile.Format.Channels * wavfile.Frames;
@@ -86,12 +98,16 @@ namespace Client
                 }
             }
 
+
             // frequency of the sound file
             int freq = wavfile.Format.SampleRate;
 
             // dispose file descriptor
             wavfile.Close();
             wavfile.Dispose();
+
+            // unsupported file type, so return
+            if (subtype == 0) return;
 
             // create a openAL buffer
             int buffer = AL.GenBuffer();
@@ -111,8 +127,18 @@ namespace Client
             if (!_audioBuffers.ContainsKey(fileName))
             {
                 GenBuffer(fileName);
+                if (!_audioBuffers.ContainsKey(fileName))
+                {
+                    Console.WriteLine("Invalid Audio File: " + fileName);
+                    return;
+                }
+
+                buffer = _audioBuffers[fileName];
             }
-            buffer = _audioBuffers[fileName];
+            else
+            {
+                buffer = _audioBuffers[fileName];
+            }
 
             // bind the buffer to the source
             AL.BindBufferToSource(soundSource, buffer);
@@ -124,6 +150,48 @@ namespace Client
             AL.SourcePlay(soundSource);
         }
 
+        /// <summary>
+        /// Stop playing the sound source
+        /// </summary>
+        /// <param name="soundSource"> source ID to be stopped </param>
+        public void Stop(int soundSource)
+        {
+            AL.SourceStop(soundSource);
+        }
+
+        /// <summary>
+        /// Pause playing the sound source
+        /// </summary>
+        /// <param name="soundSource"> source ID to be paused </param>
+        public void Pause(int soundSource)
+        {
+            AL.SourcePause(soundSource);
+        }
+
+        /// <summary>
+        /// Resume Playing the sound source
+        /// </summary>
+        /// <param name="soundSource"> source ID to be resumed </param>
+        public void Resume(int soundSource)
+        {
+            if (AL.GetSourceState(soundSource) == ALSourceState.Paused)
+            {
+                AL.SourcePlay(soundSource);
+            }
+        }
+
+        /// <summary>
+        /// Rewind the sound source
+        /// </summary>
+        /// <param name="soundSource"> source ID to be rewinded </param>
+        public void Rewind(int soundSource)
+        {
+            AL.SourceRewind(soundSource);
+        }
+
+        /// <summary>
+        /// Dispose
+        /// </summary>
         public void Dispose()
         {
             _context?.Dispose();

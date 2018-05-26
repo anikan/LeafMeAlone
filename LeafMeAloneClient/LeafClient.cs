f4d579025d7c14949ac25131261dec5897d79f1e
Nick Crow <nicholas@nicholasjcrow.com>
2018-05-25 20:02:21 -0700

    Burningggg

--- LeafMeAloneClient/LeafClient.cs
+++ LeafMeAloneClient/LeafClient.cs
@@ -17,38 +17,9 @@ namespace Client
     {
         public LeafClient(CreateObjectPacket createPacket) :
             base(createPacket, Constants.LeafModel)
-        {}
-     
-
-        /// <summary>
-        /// Update from a server packet.
-        /// </summary>
-        /// <param name="packet">Packet from the server.</param>
-        public override void UpdateFromPacket(BasePacket packet)
         {
-            ObjectPacket objPacket = packet as ObjectPacket;
-            // Set the initial positions of the object.
-
-            Transform.Position.X = objPacket.PositionX;
-            Transform.Position.Z = objPacket.PositionZ;
-            Transform.Rotation.Y = objPacket.Rotation;
-            // Set the initial burning status.
-            Burning = objPacket.Burning;
-
-            var oldHealth = Health;
-            Health = objPacket.Health;
-
-            //If the player is burning then change the leaf color.
-            if (Burning)
-            {
-                //change of health
-                var deltaHealth = oldHealth-Health;
-                var maxHealth = 5.0f;
-                CurrentTint -= new Vector3((deltaHealth / maxHealth), (deltaHealth / maxHealth), (deltaHealth / maxHealth));
-
-            }
-
         }
+
 
     }
 }

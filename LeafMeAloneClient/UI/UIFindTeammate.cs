504475f0d96f8ec709546c39926d6f57ca6d1939
Anish Kannan <anikan33@gmail.com>
2018-06-06 15:06:51 -0700

    Added first test of teammate indicator

--- LeafMeAloneClient/UI/UIFindTeammate.cs
+++ LeafMeAloneClient/UI/UIFindTeammate.cs
@@ -7,6 +7,8 @@ namespace Client.UI
     {
         public PlayerClient Teammate;
 
+        private float padding = 30.0f;
+
         public UIFindTeammate() : base(Constants.Arrow, new Vector2(100, 100),
             new Vector2(75, 75), 0)
         {
@@ -66,59 +68,65 @@ namespace Client.UI
                 {
 
                     //this actually should be true, but since this is going on master make it false for now.
-                    UITexture.Enabled = false;
-
+                    UITexture.Enabled = true;
+                    UITexture.Position = new Vector2(400, 200);
+                    UITexture.Size = new Vector2(200, 200);
                     
-                   // //Vector2 teammatePos = GraphicsManager.WorldToScreenPoint(Teammate.Transform.Position);
-                   // //Vector2 myPos = GraphicsManager.WorldToScreenPoint(GraphicsManager.ActivePlayer.Transform.Position);
-                   // //var direction = myPos - teammatePos;
-                    
+                    Vector2 teammatePos = GraphicsManager.WorldToScreenPoint(Teammate.Transform.Position);
+                    //Vector2 myPos = GraphicsManager.WorldToScreenPoint(GraphicsManager.ActivePlayer.Transform.Position);
+                    //Vector2 vectorToTeammate= teammatePos - myPos;
 
-                   // //teammatePos -= new Vector2(.5f, .5f);
-                   // //teammatePos *= 2;
-                   // //var max = Math.Max(Math.Abs(teammatePos.X), Math.Abs(teammatePos.Y));
-                   // //teammatePos = (teammatePos / (max * 2.0f)) + new Vector2(.5f, .5f);
+                    UITexture.Position = teammatePos;
 
-                   // //teammatePos.X *= Screen.Width;
-                   // //teammatePos.Y *= Screen.Height;
-
-                   // //teammatePos.X -= teammateFinder.Size.X;
-                   // //teammatePos.Y -= teammateFinder.Size.Y;
-
-                   // //teammateFinder.Position = teammatePos;
-                   // //Debug.Log(teammatePos.ToString());
-
-                   // var range = new Vector2(Screen.Width/6f, Screen.Height/6f);
-
-                   // var teammatePos = Teammate.Transform.Position;
-                   // var myPos = GraphicsManager.ActivePlayer.Transform.Position;
-                   // var direction = teammatePos - myPos;
-                   // direction.Normalize();
-                   // direction.Y -= .1f;
-
-                   // var indPos = new Vector2(range.X * direction.X,range.Y *direction.Y);
-                   // indPos = new Vector2(indPos.X + Screen.Width/2f, indPos.Y + Screen.Height / 2f);
-
-                   // //var pdir = myPos - GraphicsManager.ScreenToWorldPoint(new Vector2(indPos.X, indPos.Y));
-                   //// pdir.Normalize();
-
-                   // //float angle = ((float)Math.Atan2(pdir.X, pdir.Y));
-
-                   // UITexture.Position = indPos;
-                   // Debug.Log(indPos.ToString());
-
+                    UITexture.Position = Vector2.Clamp(UITexture.Position, new Vector2(padding, padding), new Vector2(Screen.Width - padding, Screen.Height - padding));
+                        
                     
 
-                   // //Vector2 teammatePos = GraphicsManager.WorldToScreenPoint(Teammate.Transform.Position);
-                   // //Vector2 myPos = GraphicsManager.WorldToScreenPoint(GraphicsManager.ActivePlayer.Transform.Position);
+                    // //teammatePos -= new Vector2(.5f, .5f);
+                    // //teammatePos *= 2;
+                    // //var max = Math.Max(Math.Abs(teammatePos.X), Math.Abs(teammatePos.Y));
+                    // //teammatePos = (teammatePos / (max * 2.0f)) + new Vector2(.5f, .5f);
 
-                   // //Vector2 vectortoTeammate = myPos - teammatePos;
-                   // //Vector2 newPos = IntersectScreenEdge(vectortoTeammate);
+                    // //teammatePos.X *= Screen.Width;
+                    // //teammatePos.Y *= Screen.Height;
 
-                   // //newPos.X -= teammateFinder.Size.X;
-                   // //newPos.Y -= teammateFinder.Size.Y;
-                   // //teammateFinder.Position = newPos;
-                   // //Debug.Log(newPos.ToString());
+                    // //teammatePos.X -= teammateFinder.Size.X;
+                    // //teammatePos.Y -= teammateFinder.Size.Y;
+
+                    // //teammateFinder.Position = teammatePos;
+                    // //Debug.Log(teammatePos.ToString());
+
+                    // var range = new Vector2(Screen.Width/6f, Screen.Height/6f);
+
+                    // var teammatePos = Teammate.Transform.Position;
+                    // var myPos = GraphicsManager.ActivePlayer.Transform.Position;
+                    // var direction = teammatePos - myPos;
+                    // direction.Normalize();
+                    // direction.Y -= .1f;
+
+                    // var indPos = new Vector2(range.X * direction.X,range.Y *direction.Y);
+                    // indPos = new Vector2(indPos.X + Screen.Width/2f, indPos.Y + Screen.Height / 2f);
+
+                    // //var pdir = myPos - GraphicsManager.ScreenToWorldPoint(new Vector2(indPos.X, indPos.Y));
+                    //// pdir.Normalize();
+
+                    // //float angle = ((float)Math.Atan2(pdir.X, pdir.Y));
+
+                    // UITexture.Position = indPos;
+                    // Debug.Log(indPos.ToString());
+
+
+
+                    // //Vector2 teammatePos = GraphicsManager.WorldToScreenPoint(Teammate.Transform.Position);
+                    // //Vector2 myPos = GraphicsManager.WorldToScreenPoint(GraphicsManager.ActivePlayer.Transform.Position);
+
+                    // //Vector2 vectortoTeammate = myPos - teammatePos;
+                    // //Vector2 newPos = IntersectScreenEdge(vectortoTeammate);
+
+                    // //newPos.X -= teammateFinder.Size.X;
+                    // //newPos.Y -= teammateFinder.Size.Y;
+                    // //teammateFinder.Position = newPos;
+                    // //Debug.Log(newPos.ToString());
                 }
                 else
                 {
